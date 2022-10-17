409a5883a4be6d0f7fb9832590b9e338425922f0
AlexLoup <a.loupoueyou@icloud.com>
2022-10-17 22:07:15 +0200

    [Add]: tests/game_test_alexandre.lou-poueyou@etu.u-bordeaux.fr.c

---
+++ tests/game_test_alexandre.lou-poueyou@etu.u-bordeaux.fr.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void usage(int argc, char* argv[]){
+    fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char* argv[]){
+    if (argc == 1){
+        usage(argc, argv);
+    }
+    bool ok = false;
+    if (strcmp(argv[1], "dummy") == 0){
+        ok = true;
+    }
+
+    //Print test result
+    if (ok){
+        fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
+        return EXIT_SUCCESS;
+    }
+    else{
+        fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+}
