bf98e9a65fd3e7d0d2de50a9bdfd8a77a0e69a58
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2022-10-18 09:23:05 +0200

    [Clean]: Clean game_test_alexandre test code

--- tests/game_test_alexandre.lou-poueyou@etu.u-bordeaux.fr.c
+++ tests/game_test_alexandre.lou-poueyou@etu.u-bordeaux.fr.c
@@ -3,26 +3,23 @@
 #include <stdbool.h>
 #include <string.h>
 
-void usage(int argc, char* argv[]){
+void usage(int argc, char* argv[]) {
     fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char* argv[]){
-    if (argc == 1){
+int main(int argc, char* argv[]) {
+    if (argc == 1)
         usage(argc, argv);
-    }
     bool ok = false;
-    if (strcmp(argv[1], "dummy") == 0){
+    if (strcmp(argv[1], "dummy") == 0)
         ok = true;
-    }
 
     //Print test result
-    if (ok){
+    if (ok) {
         fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
         return EXIT_SUCCESS;
-    }
-    else{
+    } else {
         fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
         return EXIT_FAILURE;
     }
