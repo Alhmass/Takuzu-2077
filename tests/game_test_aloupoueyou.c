4f89d3cfcb4b47e9c4fa2109e8f7fe2bb74ec0ff
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2022-10-22 18:42:46 +0200

    [Update]: add dummy function

--- tests/game_test_aloupoueyou.c
+++ tests/game_test_aloupoueyou.c
@@ -3,24 +3,25 @@
 #include <stdbool.h>
 #include <string.h>
 
-void usage(int argc, char* argv[]) {
+void usage(int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char* argv[]) {
+bool test_dummy() {
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
     if (argc == 1)
         usage(argc, argv);
-    bool ok = false;
-    if (strcmp(argv[1], "dummy") == 0)
-        ok = true;
-
-    //Print test result
-    if (ok) {
-        fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
-        return EXIT_SUCCESS;
-    } else {
-        fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
-        return EXIT_FAILURE;
+    if (strcmp(argv[1], "dummy") == 0) {
+        if (test_dummy()) {
+            fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
+            return EXIT_SUCCESS;
+        }
     }
+    fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
+    return EXIT_FAILURE;
 }
