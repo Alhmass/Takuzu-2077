b5f322eba77500eccfeab5d539f2cee42402d61b
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2022-10-22 18:38:49 +0200

    [Update]: modified test name
    Update CMakelists rules to compiles executable & test

---
+++ game_test_aloupoueyou.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+void usage(int argc, char *argv[]) {
+    fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+bool test_dummy() {
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+        usage(argc, argv);
+    if (strcmp(argv[1], "dummy") == 0) {
+        if (test_dummy()) {
+            fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
+            return EXIT_SUCCESS;
+        }
+    }
+    fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
+    return EXIT_FAILURE;
+}
