315362cadcce65bf39995f5dcfe6d892bc2e1bc7
Tim Lacault <tim.lacault@etu.u-bordeaux.fr>
2022-10-24 12:15:18 +0200

    [Remove]: removed dummy test in alex tests

--- tests/game_test_aloupoueyou.c
+++ tests/game_test_aloupoueyou.c
@@ -8,10 +8,6 @@ void usage(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
 }
 
-bool test_dummy() {
-    return false;
-}
-
 bool test_game_is_immutable() {
     return false;
 }
@@ -35,29 +31,27 @@ bool test_game_is_over() {
 int main(int argc, char *argv[]) {
     if (argc == 1)
         usage(argc, argv);
+
     bool ok = false;
-    if (strcmp(argv[1], "dummy") == 0) {
-        ok = test_dummy();
-    }else if (strcmp(argv[1], "game_is_immutable") == 0) {
+    if (strcmp(argv[1], "game_is_immutable") == 0) {
         ok = test_game_is_immutable();
-    }else if (strcmp(argv[1], "game_has_error") == 0) {
-        ok = test_game_has_error();
-    }else if(strcmp(argv[1], "game_check_move") == 0) {
-        ok = test_game_check_move();
-    }else if (strcmp(argv[1], "game_play_move") == 0) {
-        ok = test_game_play_move();
-    }else if (strcmp(argv[1], "game_is_over") == 0) {
-        ok = test_game_is_over();
-    }
-    else {
+    } else if (strcmp(argv[1], "game_has_error") == 0) {
+         ok = test_game_has_error();
+    } else if(strcmp(argv[1], "game_check_move") == 0) {
+         ok = test_game_check_move();
+    } else if (strcmp(argv[1], "game_play_move") == 0) {
+         ok = test_game_play_move();
+    } else if (strcmp(argv[1], "game_is_over") == 0) {
+         ok = test_game_is_over();
+    } else {
         fprintf(stderr, "Error: test \"%s\" not found\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    if (ok){
-    fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
-    return EXIT_SUCCESS;
-    } else{
-    fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
-    return EXIT_FAILURE;
+    if (ok) {
+        fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
+        return EXIT_SUCCESS;
+    } else {
+        fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
+        return EXIT_FAILURE;
     }
 }
