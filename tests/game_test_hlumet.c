cdfc4efdb9cadb2770e84a7e7d5e74cf89db02b4
Hugo Lumet <hugo.lumet@etu.u-bordeaux.fr>
2022-10-24 11:19:07 +0200

    [Update]:game_test_hlumet.c
    Adding multi testing supports and edit dummy test

--- tests/game_test_hlumet.c
+++ tests/game_test_hlumet.c
@@ -1,18 +1,42 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "game.h"
+#include "game_aux.h"
 
-void test_dummy(int argc, char *argv[])
+bool test_dummy(int argc, char *argv[])
 {
-    if (argc < 2)
-        exit(EXIT_FAILURE);
-    else if (strcmp(argv[1], "dummy") == 0)
-        exit(EXIT_SUCCESS);
-    else 
-        exit(EXIT_FAILURE);
+    return (true);
+}
+
+void usage(int argc, char *argv[])
+{
+  fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[])
 {
-    test_dummy(argc, argv);
-    return EXIT_SUCCESS;
+    if (argc == 1)
+        usage(argc, argv);
+    // start test
+    fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
+    bool ok = false;
+    if (strcmp("dummy", argv[1]) == 0)
+        ok = test_dummy(argc, argv);
+    else {
+        fprintf(stderr, "Test not found!\n");
+        exit(EXIT_FAILURE);
+    }
+    // print test result
+    if (ok) {
+        fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
+        return (EXIT_SUCCESS);
+    }
+    else {
+        fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
+        return (EXIT_FAILURE);
+    }
+    return (EXIT_SUCCESS);
 }
