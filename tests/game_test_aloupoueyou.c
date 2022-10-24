7d4a6521e294a9a025f14de67d7468556230a1c1
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2022-10-24 11:41:54 +0200

    [Update]: add tests

--- tests/game_test_aloupoueyou.c
+++ tests/game_test_aloupoueyou.c
@@ -9,19 +9,51 @@ void usage(int argc, char *argv[]) {
 }
 
 bool test_dummy() {
-    return true;
+    return false;
 }
 
-int main(int argc, char *argv[])
-{
+bool test_game_is_immutable() {
+    return false;
+}
+
+bool test_game_has_error() {
+    return false;
+}
+
+bool test_game_play_move() {
+    return false;
+}
+
+bool test_game_is_over() {
+    return false; 
+}
+
+int main(int argc, char *argv[]) {
     if (argc == 1)
         usage(argc, argv);
+    bool ok = false;
     if (strcmp(argv[1], "dummy") == 0) {
-        if (test_dummy()) {
-            fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
-            return EXIT_SUCCESS;
-        }
+        ok = test_dummy();
+    }else if (strcmp(argv[1], "game_is_immutable") == 0) {
+        ok = test_game_is_immutable(); 
+    }else if (strcmp(argv[1], "game_has_error") == 0) {
+        ok = test_game_has_error();
+    }else if(strcmp(argv[1], "game_check_move") == 0) {
+        ok = test_game_check_move();
+    }else if (strcmp(argv[1], "game_play_move") == 0) {
+        ok = test_game_play_move();
+    }else if (strcmp(argv[1], "game_is_over") == 0) {
+        ok = test_game_is_over();
     }
+    else {
+        fprintf(stderr, "Error: test \"%s\" not found\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    if (ok){        
+    fprintf(stderr, "Test: \"%s\" finished: SUCCESS\n", argv[1]);
+    return EXIT_SUCCESS;
+    } else{
     fprintf(stderr, "Test: \"%s\" finished: FAILURE\n", argv[1]);
     return EXIT_FAILURE;
+    }
 }
