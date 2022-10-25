42b5fef7c2b4f4563f57e7f94891e74e51e97ba3
Tim Lacault <tim.lacault@etu.u-bordeaux.fr>
2022-10-25 16:37:16 +0200

    [Add]: added game_is_empty test (related to #8)

--- tests/game_test_tlacault.c
+++ tests/game_test_tlacault.c
@@ -9,12 +9,12 @@
 /*  TEST  */
 bool test_game_new(void) {
     int squares[DEFAULT_SIZE][DEFAULT_SIZE] = {
-        {0, 2, 1, 0, 0, 0},
+        {0, 4, 3, 0, 0, 0},
         {0, 0, 0, 0, 0, 0},
-        {0, 1, 0, 0, 1, 0},
-        {0, 1, 2, 0, 0, 0},
-        {0, 0, 2, 0, 0, 1},
-        {0, 0, 0, 0, 0, 1}};
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 0, 0, 0},
+        {0, 0, 4, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3}};
     game g = game_new((square *)squares);
 
     if (g == NULL)
@@ -41,12 +41,12 @@ bool test_game_new_empty(void) {
 
 bool test_game_copy(void) {
     int squares[DEFAULT_SIZE][DEFAULT_SIZE] = {
-        {0, 2, 1, 0, 0, 0},
+        {0, 4, 3, 0, 0, 0},
         {0, 0, 0, 0, 0, 0},
-        {0, 1, 0, 0, 1, 0},
-        {0, 1, 2, 0, 0, 0},
-        {0, 0, 2, 0, 0, 1},
-        {0, 0, 0, 0, 0, 1}};
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 0, 0, 0},
+        {0, 0, 4, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3}};
     game g = game_new((square *)squares);
     game copy = game_copy(g);
 
@@ -61,22 +61,22 @@ bool test_game_copy(void) {
 
 bool test_game_equal(void) {
     int squares[DEFAULT_SIZE][DEFAULT_SIZE] = {
-        {0, 2, 1, 0, 0, 0},
+        {0, 4, 3, 0, 0, 0},
         {0, 0, 0, 0, 0, 0},
-        {0, 1, 0, 0, 1, 0},
-        {0, 1, 2, 0, 0, 0},
-        {0, 0, 2, 0, 0, 1},
-        {0, 0, 0, 0, 0, 1}};
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 0, 0, 0},
+        {0, 0, 4, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3}};
     game g1 = game_new((square *)squares);
     game g2 = game_copy(g1);
 
     int squares_other[DEFAULT_SIZE][DEFAULT_SIZE] = {
-        {0, 2, 1, 0, 0, 0},
-        {0, 0, 0, 0, 4, 0},
-        {0, 1, 0, 0, 1, 0},
-        {0, 1, 2, 1, 0, 0},
-        {0, 0, 2, 0, 0, 1},
-        {3, 0, 0, 0, 0, 2}};
+        {0, 4, 3, 0, 0, 0},
+        {0, 0, 0, 2, 4, 0},
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 3, 0, 0},
+        {0, 0, 2, 0, 0, 3},
+        {3, 0, 0, 1, 0, 4}};
     game g3 = game_new((square *)squares_other);
 
     if (!game_equal(g1, g2) && game_equal(g1, g3))
@@ -85,13 +85,24 @@ bool test_game_equal(void) {
 }
 
 bool test_game_delete(void) {
-    // game g
-    return false;
+    return true;
 }
 
 bool test_game_is_empty(void) {
-    // cgame g, uint i, uint j
-    return false;
+    int squares[DEFAULT_SIZE][DEFAULT_SIZE] = {
+        {0, 4, 3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 0, 0, 0},
+        {0, 0, 4, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3}};
+    game g = game_new((square *)squares);
+
+    for (uint i = 0; i < DEFAULT_SIZE; i++)
+        for (uint j = 0; j < DEFAULT_SIZE; j++)
+            if (squares[i][j] == 0 && game_is_empty(g, i, j) == false)
+                return false;
+    return true;
 }
 
 bool test_game_restart(void) {
