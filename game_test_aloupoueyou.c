a1e3d536527da04c69b6516577572c2baf529d1a
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2022-10-26 15:48:49 +0200

    [Update]: Update game_is_immutable test (related to #7)

--- game_test_aloupoueyou.c
+++ game_test_aloupoueyou.c
@@ -1,20 +1,10 @@
-/*
-  ╭────────────────────────────────────────────────────────╮
-    > File:           game_test_aloupoueyou.c
-    > Created Date:   25-10-2022 17:10
-    > Author:         Tim Lacault
-    ──────────────────────────────────────────────────────
-    > Last Modified:  25-10-2022 17:10
-    > Modified By:    Tim Lacault
-    ──────────────────────────────────────────────────────
-    > Description:
-  ╰────────────────────────────────────────────────────────╯
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+
+#include "game_aux.h"
+#include "game.h"
 
 void usage(int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
@@ -22,7 +12,24 @@ void usage(int argc, char *argv[]) {
 }
 
 bool test_game_is_immutable() {
-    return false;
+    int squares[DEFAULT_SIZE][DEFAULT_SIZE] = {
+        {0, 4, 3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 0, 0, 0},
+        {0, 0, 4, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3}};
+    game g = game_new((square *)squares);
+    for (uint i = 0; i < DEFAULT_SIZE; i++) {
+        for (uint j = 0; j < DEFAULT_SIZE; j++) {
+            if (game_is_immutable(g, i, j) && (game_get_square(g, i, j) != S_IMMUTABLE_ONE && game_get_square(g, i, j) != S_IMMUTABLE_ZERO)) {
+                return false;
+            } else if (!(game_is_immutable(g, i, j)) && (game_get_square(g, i, j) == S_IMMUTABLE_ONE || game_get_square(g, i, j) == S_IMMUTABLE_ZERO)) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 bool test_game_has_error() {
--- tests/game_test_aloupoueyou.c
+++ tests/game_test_aloupoueyou.c
@@ -3,13 +3,33 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "game_aux.h"
+#include "game.h"
+
 void usage(int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
     exit(EXIT_FAILURE);
 }
 
 bool test_game_is_immutable() {
-    return false;
+    int squares[DEFAULT_SIZE][DEFAULT_SIZE] = {
+        {0, 4, 3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 0, 0, 0},
+        {0, 0, 4, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3}};
+    game g = game_new((square *)squares);
+    for (uint i = 0; i < DEFAULT_SIZE; i++) {
+        for (uint j = 0; j < DEFAULT_SIZE; j++) {
+            if (game_is_immutable(g, i, j) && (game_get_square(g, i, j) != S_IMMUTABLE_ONE && game_get_square(g, i, j) != S_IMMUTABLE_ZERO)) {
+                return false;
+            } else if (!(game_is_immutable(g, i, j)) && (game_get_square(g, i, j) == S_IMMUTABLE_ONE || game_get_square(g, i, j) == S_IMMUTABLE_ZERO)) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 bool test_game_has_error() {
