c041133a51f2665979bcef5213f1aea1e31eec95
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2022-10-30 22:21:32 +0100

    [Update]: game_check_move test (related to #13)

--- tests/game_test_aloupoueyou.c
+++ tests/game_test_aloupoueyou.c
@@ -33,11 +33,29 @@ bool test_game_is_immutable() {
 }
 
 bool test_game_has_error() {
+    
     return false;
 }
 
 bool test_game_check_move() {
-    return false;
+    int squares[DEFAULT_SIZE][DEFAULT_SIZE] = {
+        {0, 4, 3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 3, 0, 0, 3, 0},
+        {0, 3, 4, 0, 0, 0},
+        {0, 0, 4, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3}};
+    game g = game_new((square *)squares);
+    for (uint i = 0; i < DEFAULT_SIZE; i++) {
+        for (uint j = 0; j < DEFAULT_SIZE; j++) {
+            if (game_check_move(g, i, j, S_EMPTY) && (game_get_square(g, i, j) == S_IMMUTABLE_ONE || game_get_square(g, i, j) == S_IMMUTABLE_ZERO)) {
+                return false;
+            } else if (!(game_check_move(g, i, j, S_EMPTY)) && (game_get_square(g, i, j) != S_IMMUTABLE_ONE && game_get_square(g, i, j) != S_IMMUTABLE_ZERO)) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 bool test_game_play_move() {
