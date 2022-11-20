edea9184c49d1f63144f09b00be8d6534194c334
Tim Lacault <contact@dev-tlacault.eu>
2022-11-20 16:29:45 +0100

    [Update]: implemented game_print and default (related to #26 #28)

--- game_aux.c
+++ game_aux.c
@@ -1,8 +1,40 @@
-#include <stddef.h>
 #include "game_aux.h"
 
-void game_print(cgame g) { return; }
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-game game_default(void) { return NULL; }
+void throw_error(char *msg);
+
+void game_print(cgame g) {
+    if (g == NULL)
+        throw_error("game is NULL");
+    printf("   012345\n   ------\n");
+    for (int i = 0; i < DEFAULT_SIZE; i++) {
+        printf("%d |", i);
+        for (int j = 0; j < DEFAULT_SIZE; j++) {
+            square s = game_get_square(g, i, j);
+            if (s == S_EMPTY)
+                printf(" ");
+            else if (s == S_ZERO)
+                printf("w");
+            else if (s == S_ONE)
+                printf("b");
+            else if (s == S_IMMUTABLE_ZERO)
+                printf("W");
+            else if (s == S_IMMUTABLE_ONE)
+                printf("B");
+        }
+        printf("|\n");
+    }
+    printf("   ------\n");
+}
+
+game game_default(void) {
+    square squares[DEFAULT_SIZE * DEFAULT_SIZE] = {0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0,
+                                                   0, 3, 4, 0, 0, 0, 0, 0, 4, 0, 0, 3, 0, 0, 0, 0, 0, 3};
+    return game_new(squares);
+}
 
 game game_default_solution(void) { return NULL; }
