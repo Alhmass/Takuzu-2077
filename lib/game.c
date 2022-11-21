0726262e64645b2b94de5660218f387a378ada9a
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2022-11-21 11:20:40 +0100

    [Update]: game.c (related to #29)

--- lib/game.c
+++ lib/game.c
@@ -129,6 +129,7 @@ bool game_is_immutable(cgame g, uint i, uint j) {
     if (!g){
         throw_error("game g is not initialized");
     }
+    assert(((i <= 0 && i < DEFAULT_SIZE) && (j <= 0 && j < DEFAULT_SIZE)));
     square s = game_get_square(g, i, j);
     if (s == 3 || s == 4) {
         return true;
