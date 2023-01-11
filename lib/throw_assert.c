3a59a3af61a41923fd633734b3739005686b8476
Théo Mainguenaud <theo.mainguenaud@etu.u-bordeaux.fr>
2023-01-11 23:16:53 +0100

    Refactoring throw_assert functions
    Refactoring[throw_assert]: null conditions refactoring

--- lib/throw_assert.c
+++ lib/throw_assert.c
@@ -1,26 +1,26 @@
 #include "takuzu.h"
 
 bool throw_error(char *msg) {
-    if (msg == NULL)
+    if (!msg)
         return false;
     fprintf(stderr, "[error] %s\n", msg);
     exit(EXIT_FAILURE);
 }
 
 bool game_test(game g, char *msg) {
-    if (g == NULL)
+    if (!g)
         throw_error(msg);
     return true;
 }
 
 bool cgame_test(cgame g, char *msg) {
-    if (g == NULL)
+    if (!g)
         throw_error(msg);
     return true;
 }
 
 bool pointer_test(void *ptr, char *msg) {
-    if (ptr == NULL)
+    if (!ptr)
         throw_error(msg);
     return true;
 }
