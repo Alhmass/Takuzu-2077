bc354b6679e2099ed88f4f8a091111e16eb9be3d
Tim Lacault <tim.lacault@etu.u-bordeaux.fr>
2023-01-11 23:39:03 +0100

    [Fix]: fixed MemCheck for undo & redo

--- tests/test_game_ext.c
+++ tests/test_game_ext.c
@@ -1,67 +1,51 @@
 #include "takuzu.h"
 
 bool test_game_new_ext(void) {
+    bool pass = true;
     square squares[DEFAULT_SIZE * DEFAULT_SIZE] = {1, 4, 3, 2, 1, 2, 1, 2, 2, 1, 2, 1, 2, 3, 1, 2, 3, 2,
                                                    2, 3, 4, 1, 1, 2, 1, 2, 4, 1, 2, 3, 2, 1, 1, 2, 2, 3};
     game g1 = game_new_ext(DEFAULT_SIZE, DEFAULT_SIZE, squares, true, true);
     game g2 = game_new_ext(DEFAULT_SIZE, DEFAULT_SIZE, squares, false, false);
     if (g1 == NULL || g2 == NULL)
-        return false;
-
+        pass = false;
     if (game_nb_rows(g1) != DEFAULT_SIZE || game_nb_cols(g1) != DEFAULT_SIZE || !game_is_wrapping(g1) ||
-        !game_is_unique(g1)) {
-        game_delete(g1);
-        game_delete(g2);
-        return false;
-    }
+        !game_is_unique(g1))
+        pass = false;
     if (game_nb_rows(g2) != DEFAULT_SIZE || game_nb_cols(g2) != DEFAULT_SIZE || game_is_wrapping(g2) ||
-        game_is_unique(g2)) {
-        game_delete(g1);
-        game_delete(g2);
-        return false;
-    }
-    if (!game_equal(g1, g2)) {
-        game_delete(g1);
-        game_delete(g2);
-        return false;
-    }
+        game_is_unique(g2))
+        pass = false;
+    if (!game_equal(g1, g2))
+        pass = false;
     game_delete(g1);
     game_delete(g2);
-    return true;
+    return pass;
 }
 
 bool test_game_new_empty_ext(void) {
+    bool pass = true;
     game g = game_new_empty_ext(DEFAULT_SIZE, DEFAULT_SIZE, 1, 1);
     game empty = game_new_empty();
     if (g == NULL || empty == NULL)
-        return false;
+        pass = false;
     if (game_nb_rows(g) != DEFAULT_SIZE || game_nb_cols(g) != DEFAULT_SIZE || !game_is_wrapping(g) ||
-        !game_is_unique(g)) {
-        game_delete(empty);
-        game_delete(g);
-        return false;
-    }
-    if (!game_equal(g, empty)) {
-        game_delete(empty);
-        game_delete(g);
-        return false;
-    }
+        !game_is_unique(g))
+        pass = false;
+    if (!game_equal(g, empty))
+        pass = false;
     game_delete(empty);
     game_delete(g);
-    return true;
+    return pass;
 }
 
 bool test_game_undo(void) {
-    game g = game_new_empty();
+    bool pass = true;
+    game g = game_new_empty_ext(5, 5, false, false);
     game_play_move(g, 0, 0, S_ONE);
     square before = game_get_square(g, 0, 0);
     game_undo(g);
     square after = game_get_square(g, 0, 0);
-    if (before != after) {
-        game_delete(g);
-        return false;
-    }
-
+    if (before != S_ONE || after != S_EMPTY)
+        pass = false;
     game_play_move(g, 0, 0, S_ONE);
     game_play_move(g, 0, 1, S_ONE);
     game_play_move(g, 0, 2, S_ONE);
@@ -71,17 +55,15 @@ bool test_game_undo(void) {
     game_undo(g);
     square after2 = game_get_square(g, 0, 1);
     square after3 = game_get_square(g, 0, 2);
-    if (before2 != after2 || before3 != after3) {
-        game_delete(g);
-        return false;
-    }
-
+    if (before2 == after2 || before3 == after3)
+        pass = false;
     game_delete(g);
-    return true;
+    return pass;
 }
 
 bool test_game_redo(void) {
-    game g = game_new_empty();
+    bool pass = true;
+    game g = game_new_empty_ext(5, 5, false, false);
     game_play_move(g, 0, 0, S_ONE);
     game_play_move(g, 0, 1, S_ONE);
     game_play_move(g, 0, 2, S_ONE);
@@ -93,78 +75,71 @@ bool test_game_redo(void) {
     game_redo(g);
     square after = game_get_square(g, 0, 1);
     square after2 = game_get_square(g, 0, 2);
-    if (before != after || before2 != after2) {
-        game_delete(g);
-        return false;
-    }
-
+    if (before == after || before2 == after2)
+        pass = false;
     game_play_move(g, 1, 0, S_ONE);
     game_play_move(g, 1, 1, S_ONE);
     game_play_move(g, 1, 2, S_ONE);
     game_undo(g);
     game_undo(g);
     game_play_move(g, 2, 0, S_ONE);
-    square before3 = game_get_square(g, 1, 1);
+    game_play_move(g, 2, 0, S_ZERO);
+    game_undo(g);
+    square before3 = game_get_square(g, 2, 0);
     game_redo(g);
-    square after3 = game_get_square(g, 1, 1);
-    if (before3 != after3) {
-        game_delete(g);
-        return false;
-    }
+    square after3 = game_get_square(g, 2, 0);
+    if (before3 == after3)
+        pass = false;
     game_delete(g);
-    return true;
+    return pass;
 }
 
 bool test_game_nb_rows(void) {
-    bool status = true;
+    bool pass = true;
     game g = game_new_empty_ext(12, 12, false, false);
     game g2 = game_new_empty_ext(8, 4, false, false);
     if (!g || !g2)
         return false;
     if (game_nb_rows(g) != 12 || game_nb_rows(g2) != 8)
-        status = false;
+        pass = false;
     game_delete(g);
     game_delete(g2);
-    return status;
+    return pass;
 }
 
 bool test_game_nb_cols(void) {
-    bool status = true;
+    bool pass = true;
     game g = game_new_empty_ext(12, 12, false, false);
     game g2 = game_new_empty_ext(8, 4, false, false);
     if (!g || !g2)
         return false;
     // if (game_nb_cols(g) != 12 || game_nb_cols(g2) != 4)
-    //     status = false;
+    //     pass = false;
     game_delete(g);
     game_delete(g2);
-    return status;
+    return pass;
 }
 
 bool test_game_is_unique(void) {
+    bool pass = true;
     game g1 = game_new_empty_ext(8, 8, false, true);
     game g2 = game_new_empty_ext(8, 8, false, false);
-    if (!game_is_unique(g1) || game_is_unique(g2)) {
-        game_delete(g1);
-        game_delete(g2);
-        return false;
-    }
+    if (!game_is_unique(g1) || game_is_unique(g2))
+        pass = false;
     game_delete(g1);
     game_delete(g2);
-    return true;
+    return pass;
 }
 
 bool test_game_is_wrapping(void) {
+    bool pass = true;
     game g1 = game_new_empty_ext(8, 8, true, false);
     game g2 = game_new_empty_ext(8, 8, false, false);
-    if (!game_is_wrapping(g1) || game_is_wrapping(g2)) {
-        game_delete(g1);
-        game_delete(g2);
+    if (!game_is_wrapping(g1) || game_is_wrapping(g2))
         return false;
-    }
     game_delete(g1);
     game_delete(g2);
-    return true;
+    return pass;
 }
 
 /*  USAGE  */
