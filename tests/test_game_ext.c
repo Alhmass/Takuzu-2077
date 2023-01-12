71300e55745bae5b930bab7413003d789544e9e0
Tim Lacault <tim.lacault@etu.u-bordeaux.fr>
2023-01-12 16:04:46 +0100

    [Update]: bug fix #23 (game_new_ext switch nb_cols & nb_rows)

--- tests/test_game_ext.c
+++ tests/test_game_ext.c
@@ -18,6 +18,15 @@ bool test_game_new_ext(void) {
         pass = false;
     game_delete(g1);
     game_delete(g2);
+
+    game g3 = game_new_ext(3, 4, squares, false, true);
+    if (g3 == NULL)
+        pass = false;
+    if (game_nb_rows(g3) == 4 || game_nb_cols(g3) == 3)
+        pass = false;
+    if (!game_is_unique(g3) || game_is_wrapping(g3))
+        pass = false;
+    game_delete(g3);
     return pass;
 }
 
