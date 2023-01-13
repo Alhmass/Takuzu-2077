89de40868709180538257b73c10738045f8eff8f
Tim Lacault <tim.lacault@etu.u-bordeaux.fr>
2023-01-13 01:58:34 +0100

    [Add]: added undo / redo / restart test_v2

--- tests/test_v2.c
+++ tests/test_v2.c
@@ -1,59 +1,114 @@
 #include "takuzu.h"
 
-bool test_new_ext() {
-    return true;
-}
+bool test_new_ext() { return true; }
 
-bool test_new_empty_ext() {
-    return true;
-}
+bool test_new_empty_ext() { return true; }
 
-bool test_equal_ext() {
-    return true;
-}
+bool test_equal_ext() { return true; }
 
-bool test_copy_ext() {
-    return true;
-}
+bool test_copy_ext() { return true; }
 
-bool test_get_next_square_wrapping() {
-    return true;
-}
+bool test_get_next_square_wrapping() { return true; }
 
-bool test_get_next_number_wrapping() {
-    return true;
-}
+bool test_get_next_number_wrapping() { return true; }
 
-bool test_has_error_wrapping() {
-    return true;
-}
+bool test_has_error_wrapping() { return true; }
 
-bool test_is_over_wrapping() {
-    return true;
-}
+bool test_is_over_wrapping() { return true; }
 
-bool test_has_error_unique() {
-    return true;
-}
+bool test_has_error_unique() { return true; }
 
-bool test_is_over_unique() {
-    return true;
-}
+bool test_is_over_unique() { return true; }
 
 bool test_undo_one() {
-    return true;
+    bool pass = true;
+    game g1 = game_new_empty_ext(4, 4, false, false);
+    game_undo(g1);
+    game_delete(g1);
+
+    game g2 = game_new_empty_ext(4, 4, false, false);
+    game_play_move(g2, 0, 0, S_ONE);
+    game_undo(g2);
+    if (game_get_square(g2, 0, 0) != S_EMPTY)
+        pass = false;
+    game_delete(g2);
+
+    game g3 = game_new_empty_ext(4, 4, false, false);
+    game_play_move(g3, 0, 0, S_ONE);
+    game_play_move(g3, 0, 1, S_ONE);
+    game_undo(g3);
+    if (game_get_square(g3, 0, 0) != S_ONE || game_get_square(g3, 0, 1) != S_EMPTY)
+        pass = false;
+    game_delete(g3);
+
+    game g4 = game_new_empty_ext(4, 4, false, false);
+    for (int i = 0; i < 20; i++) {
+        game_play_move(g4, 2, 2, S_ONE);
+        game_undo(g4);
+    }
+    if (game_get_square(g4, 2, 2) != S_EMPTY)
+        pass = false;
+    game_delete(g4);
+    return pass;
 }
 
 bool test_undo_redo_some() {
-    return true;
+    bool pass = true;
+    game g1 = game_new_empty_ext(4, 4, false, false);
+    game_play_move(g1, 0, 0, S_ONE);
+    game_play_move(g1, 0, 1, S_ONE);
+    game_play_move(g1, 0, 2, S_ONE);
+    game_play_move(g1, 0, 3, S_ONE);
+    game_undo(g1);
+    game_undo(g1);
+    game_undo(g1);
+    if (game_get_square(g1, 0, 0) != S_ONE || game_get_square(g1, 0, 1) != S_EMPTY ||
+        game_get_square(g1, 0, 2) != S_EMPTY || game_get_square(g1, 0, 3) != S_EMPTY)
+        pass = false;
+    game_redo(g1);
+    game_redo(g1);
+    game_redo(g1);
+    if (game_get_square(g1, 0, 0) != S_ONE || game_get_square(g1, 0, 1) != S_ONE ||
+        game_get_square(g1, 0, 2) != S_ONE || game_get_square(g1, 0, 3) != S_ONE)
+        pass = false;
+    game_delete(g1);
+    return pass;
 }
 
 bool test_undo_redo_all() {
-    return true;
+    bool pass = true;
+    game g1 = game_new_empty_ext(4, 4, false, false);
+    for (int i = 0; i < 20; i++) {
+        game_play_move(g1, 2, 2, S_ONE);
+    }
+    game_play_move(g1, 2, 2, S_ZERO);
+    for (int i = 0; i < 22; i++) {
+        game_undo(g1);
+    }
+    if (game_get_square(g1, 2, 2) != S_EMPTY)
+        pass = false;
+    for (int i = 0; i < 22; i++) {
+        game_redo(g1);
+    }
+    if (game_get_square(g1, 2, 2) != S_ZERO)
+        pass = false;
+    game_delete(g1);
+    return pass;
 }
 
 bool test_restart_undo() {
-    return true;
+    bool pass = true;
+    game g1 = game_new_empty_ext(4, 4, false, false);
+    game_play_move(g1, 0, 0, S_ONE);
+    game_play_move(g1, 0, 1, S_ONE);
+    game_play_move(g1, 0, 2, S_ONE);
+    game_restart(g1);
+    game_undo(g1);
+    if (game_get_square(g1, 0, 0) != S_EMPTY || game_get_square(g1, 0, 1) != S_EMPTY ||
+        game_get_square(g1, 0, 2) != S_EMPTY)
+        pass = false;
+    game_delete(g1);
+    return pass;
 }
 
 /*  USAGE  */
