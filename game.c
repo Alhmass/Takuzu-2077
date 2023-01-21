84d0bbcff5ab0320d2a5e4a6fb4f1b1ae9301c5b
Hugo Lumet <hugo.lumet@etu.u-bordeaux.fr>
2023-01-21 11:37:50 +0100

    [Update]:game.c
    Fixing code format

--- game.c
+++ game.c
@@ -104,7 +104,7 @@ int game_get_next_square(cgame g, uint i, uint j, direction dir, uint dist) {
         throw_error("the distance value must be <=2!\n");
     uint rows_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_rows(g);
     uint cols_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_cols(g);
-    int next_index = 0; // new coordinates for i or j
+    int next_index = 0;  // new coordinates for i or j
     if (g->version == 2 && game_is_wrapping(g)) {
         if (dir == LEFT) {
             next_index = (j - dist);
@@ -139,7 +139,7 @@ int game_get_next_number(cgame g, uint i, uint j, direction dir, uint dist) {
         throw_error("the distance value must be <=2!\n");
     uint rows_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_rows(g);
     uint cols_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_cols(g);
-    int next_index = 0; // new coordinates for i or j
+    int next_index = 0;  // new coordinates for i or j
     if (g->version == 2 && game_is_wrapping(g)) {
         if (i >= rows_g || j >= cols_g)
             return (-1);
@@ -174,7 +174,7 @@ bool game_is_empty(cgame g, uint i, uint j) {
     cgame_test(g, "g is not initialized\n");
     uint rows_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_rows(g);
     uint cols_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_cols(g);
-    assert(((i < rows_g) && (j < cols_g))); // i and j must be in the range of the game
+    assert(((i < rows_g) && (j < cols_g)));  // i and j must be in the range of the game
     return (game_get_square(g, i, j) == S_EMPTY);
 }
 
@@ -182,7 +182,7 @@ bool game_is_immutable(cgame g, uint i, uint j) {
     cgame_test(g, "g is not initialized\n");
     uint rows_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_rows(g);
     uint cols_g = (g->version == 1) ? DEFAULT_SIZE : game_nb_cols(g);
-    assert(((i < rows_g) && (j < cols_g))); // i and j must be in the range of the game
+    assert(((i < rows_g) && (j < cols_g)));  // i and j must be in the range of the game
     square s = game_get_square(g, i, j);
     if (s == 3 || s == 4)
         return true;
