4e807c04e312fd32b539d15aa448743ad85e24ca
Tim Lacault <tim.lacault@etu.u-bordeaux.fr>
2023-02-02 22:28:58 +0100

    [Add]: added save action to game_text

--- game_text.c
+++ game_text.c
@@ -8,26 +8,27 @@ static void display_help(void) {
     printf("- press 'z' to undo previous move\n");
     printf("- press 'y' to redo undone moves\n");
     printf("- press 'r' to restart\n");
+    printf("- press 's' <filename> to save te game in its current state (stored in ./saves/user/<filename>)\n");
     printf("- press 'q' to quit\n");
 }
 
-static void command(game *g, char c) {
+static void command(game g, char c) {
     if (c == 'r') {
         printf("> action: restart\n");
-        game_restart(*g);
+        game_restart(g);
     } else if (c == 'h') {
         display_help();
     } else if (c == 'q') {
         printf("> action: quit\n");
         printf("What a shame you gave up :-(\n");
-        game_delete(*g);
+        game_delete(g);
         exit(EXIT_SUCCESS);
     } else if (c == 'z') {
         printf("> action: undo\n");
-        game_undo(*g);
+        game_undo(g);
     } else if (c == 'y') {
         printf("> action: redo\n");
-        game_redo(*g);
+        game_redo(g);
     }
 }
 
@@ -49,24 +50,24 @@ static square char_to_square(char c) {
         return (S_EMPTY);
 }
 
-static void try_play_move(game *g, char c, int i, int j) {
+static void try_play_move(game g, char c, int i, int j) {
     square s = char_to_square(c);
 
-    if (game_check_move(*g, i, j, s)) {
+    if (game_check_move(g, i, j, s)) {
         printf("> action: play move '%c' into square (%d,%d)\n", c, i, j);
-        game_play_move(*g, i, j, s);
+        game_play_move(g, i, j, s);
     }
 }
 
-int main(void) {
-    game g = game_load("default.txt");
+int main(int ac, char **av) {
+    game g = (ac == 2) ? game_load(av[1]) : game_load("default.txt");
+    assert(g);
 
-    char user_input;
+    char user_input = 0;
     int scanf_return = 0;
-    uint i = 0;
-    uint j = 0;
+    char *save_path = NULL;
+    uint i = 0, j = 0;
 
-    assert(g != NULL);
     while (!game_is_over(g)) {
         game_print(g);
         display_errors(g);
@@ -74,12 +75,16 @@ int main(void) {
         if (scanf_return == EOF)
             exit(EXIT_SUCCESS);
         scanf_return = scanf(" %c", &user_input);
-        if (strchr("rhqzy", user_input)) {
-            command(&g, user_input);
+        if (strchr("hzyrsq", user_input)) {
+            command(g, user_input);
         } else if (strchr("wbe", user_input)) {
             scanf_return = scanf("%d %d", &i, &j);
             if (scanf_return == 2)
-                try_play_move(&g, user_input, i, j);
+                try_play_move(g, user_input, i, j);
+        } else if (strchr("s", user_input)) {
+            scanf_return = scanf("%s", save_path);
+            if (scanf_return == 1)
+                game_save(g, save_path);
         }
     }
     game_print(g);
