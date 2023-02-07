49340a83d18ad625efefd66aa4a761bdeaede0a0
Hugo Lumet <hugo.lumet@etu.u-bordeaux.fr>
2023-02-07 10:15:58 +0100

    [Update]:test_game_tools.c
    Adding game load function test

--- tests/test_game_tools.c
+++ tests/test_game_tools.c
@@ -1,6 +1,63 @@
 #include "takuzu.h"
 
-bool test_game_load() { return false; }
+bool test_game_load() {
+    FILE *f_default = fopen("../saves/game/default.txt", "r");
+    if (f_default == NULL) {
+        return false;
+    }
+    game g_default = game_load("../saves/game/default.txt");
+    if (g_default == NULL) {
+        fclose(f_default);
+        return false;
+    }
+    uint nb_rows, nb_cols, wrapping, unique;
+    if (fscanf(f_default, "%u %u %u %u\n", &nb_rows, &nb_cols, &wrapping, &unique) != 4) {
+        fclose(f_default);
+        game_delete(g_default);
+        return false;
+    }
+    if (nb_rows != game_nb_rows(g_default) || nb_cols != game_nb_cols(g_default) ||
+        game_is_wrapping(g_default) != wrapping || game_is_unique(g_default) != unique) {
+        fclose(f_default);
+        game_delete(g_default);
+        return false;
+    }
+    for (uint i = 0; i < game_nb_cols(g_default); i++) {
+        for (uint j = 0; j < game_nb_rows(g_default); j++) {
+            char input;
+            square s;
+            if (fscanf(f_default, "%c", &input) != 1) {
+                fclose(f_default);
+                game_delete(g_default);
+                return false;
+            }
+            if (input == 'e')
+                s = S_EMPTY;
+            else if (input == 'w')
+                s = S_ZERO;
+            else if (input == 'W')
+                s = S_IMMUTABLE_ZERO;
+            else if (input == 'b')
+                s = S_ONE;
+            else if (input == 'B')
+                s = S_IMMUTABLE_ONE;
+            else {
+                fclose(f_default);
+                game_delete(g_default);
+                return false;
+            }
+            if (s != game_get_square(g_default, i, j)) {
+                fclose(f_default);
+                game_delete(g_default);
+                return false;
+            }
+        }
+        fscanf(f_default, "\n");
+    }
+    fclose(f_default);
+    game_delete(g_default);
+    return true;
+}
 
 bool test_game_game_save() { return false; }
 
