720bd962497c547589e11e3cc4418b71a04edce6
Tim Lacault <tim.lacault@etu.u-bordeaux.fr>
2023-03-19 19:46:56 +0100

    [Update]: shortened solver test (timeout)

--- game_solver.c
+++ game_solver.c
@@ -15,7 +15,7 @@ solver solver_new(game g, bool unique) {
     uint index = 0;
     for (uint i = 0; i < g->nb_cols; i++) {
         for (uint j = 0; j < g->nb_rows; j++) {
-            if (game_get_square(g, i, j) == S_EMPTY) {
+            if (game_get_square(g, i, j) != S_IMMUTABLE_ZERO && game_get_square(g, i, j) != S_IMMUTABLE_ONE) {
                 s->coords[index] = malloc(sizeof(struct coordinates_s));
                 COORDS_I(s, index) = i;
                 COORDS_J(s, index) = j;
