c34b8e53d39670d8da433a43dd3e8bcc13fdbf19
Hugo Lumet <hugo.lumet@etu.u-bordeaux.fr>
2023-03-29 01:39:11 +0200

    [ADD]:file.c, file.h
    Adding file management backend module

---
+++ include/sdl/file.h
@@ -0,0 +1,22 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LINE_SIZE(file, line) (get_pos((file), (line), '\n') - get_pos((file), (line - 1), '\n'))
+#define SETTING_SIZE(file, line) (get_pos((file), (line), ' ') - get_pos((file), (line - 1), '\n'))
+
+FILE *open_file(const char *path);
+void close_file(FILE *file);
+
+char *get_line(char *path, int line);
+char *get_setting(char *path, int line);
+
+bool set_value(char *path, int line, int value);
+bool set_setting(char *path, int line, char *value);
+bool set_line(char *path, int line, char *nline);
+
+#endif /* FILE_H */
---
+++ sdl/file.c
@@ -0,0 +1,111 @@
+#include "file.h"
+
+FILE *open_file(const char *path) {
+    FILE *f = fopen(path, "rb+");
+    assert(f);
+    return f;
+}
+
+void close_file(FILE *file) {
+    if (file != NULL)
+        fclose(file);
+}
+
+static int get_pos(FILE *file, int line, char delim) {
+    assert(file);
+    for (int i = 0; i < line; i++) {
+        while (!feof(file) && fgetc(file) != delim)
+            ;
+    }
+    int pos = ftell(file);
+    rewind(file);
+    return pos;
+}
+
+char *get_line(char *path, int line) {
+    FILE *file = open_file(path);
+    assert(file);
+    int line_size = LINE_SIZE(file, line);
+    assert(line_size >= 0);
+    char *linebuf = malloc(sizeof(char) * line_size);
+    assert(linebuf);
+    fseek(file, get_pos(file, line - 1, '\n'), SEEK_SET);
+    fgets(linebuf, line_size, file);
+    close_file(file);
+    return linebuf;
+}
+
+char *get_setting(char *path, int line) {
+    FILE *file = open_file(path);
+    int setting_size = SETTING_SIZE(file, line);
+    assert(setting_size >= 0);
+    char *buf = malloc(sizeof(char) * setting_size);
+    fseek(file, get_pos(file, line, ' '), SEEK_SET);
+    fgets(buf, setting_size, file);
+    close_file(file);
+    return (buf);
+}
+
+bool set_line(char *path, int line, char *nline) {
+    assert(nline);
+    FILE *file = open_file(path);
+    char tempFile[] = "temp.txt";
+    FILE *tmp = fopen(tempFile, "a");
+    char buffer[255];
+    int count = 0;
+    assert(tmp);
+    while (fgets(buffer, 255, file)) {
+        count++;
+        if (count != line) {
+            if (fprintf(tmp, "%s", buffer) == EOF)
+                return false;
+        } else if (count == line) {
+            if (fprintf(tmp, "%s\n", nline) == EOF)
+                return false;
+        }
+    }
+    fclose(tmp);
+    close_file(file);
+    remove(path);
+    rename(tempFile, path);
+    return true;
+}
+
+bool set_setting(char *path, int line, char *value) {
+    assert(value);
+
+    char *curr_line = get_line(path, line);
+    strtok(curr_line, " ");
+
+    size_t len = strlen(curr_line) + strlen(value) + 2;
+    char *new_line = malloc(sizeof(char) * len);
+    assert(new_line);
+    strcpy(new_line, curr_line);
+    strcat(new_line, " ");
+    strcat(new_line, value);
+
+    bool res = set_line(path, line, new_line);
+    free(curr_line);
+    free(new_line);
+    return res;
+}
+
+bool set_value(char *path, int line, int value) {
+    char str[10];
+    snprintf(str, sizeof(str), "%d", value);
+
+    char *curr_line = get_line(path, line);
+    strtok(curr_line, " ");
+
+    size_t len = strlen(curr_line) + strlen(str) + 2;
+    char *new_line = malloc(sizeof(char) * len);
+    assert(new_line);
+    strcpy(new_line, curr_line);
+    strcat(new_line, " ");
+    strcat(new_line, str);
+
+    bool res = set_line(path, line, new_line);
+    free(curr_line);
+    free(new_line);
+    return res;
+}
