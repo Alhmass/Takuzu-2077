f091fc46baca5b3f0cbdef5f6c7588f0a1e55117
Hugo Lumet <hugo.lumet@etu.u-bordeaux.fr>
2023-03-30 12:06:22 +0200

    [Update]:file.c, file.h
    Updating back end module file to get and set setting with string patern matching

--- include/sdl/file.h
+++ include/sdl/file.h
@@ -7,17 +7,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LINE_SIZE(file, line) (get_pos((file), (line), '\n') - get_pos((file), (line - 1), '\n'))
-#define SETTING_SIZE(file, line) (get_pos((file), (line), ' ') - get_pos((file), (line - 1), '\n'))
+#define LINE_SIZE(file, line) (get_pos((file), (line)) - get_pos((file), (line - 1)))
 
 FILE *open_file(const char *path);
 void close_file(FILE *file);
 
 char *get_line(char *path, int line);
-//char *get_setting(char *path, char *setting);
+char *get_setting(char *path, char *setting);
 
 bool set_value(char *path, int line, int value);
-//bool set_setting(char *path, char *setting, char *value);
+bool set_setting(char *path, char *setting, char *value);
 bool set_line(char *path, int line, char *nline);
 
 #endif /* FILE_H */
