9add7f24421ec36be725c2d2993fee5bf6bc3d44
Alexandre Lou- Poueyou <alexandre.lou-poueyou@etu.u-bordeaux.fr>
2023-03-31 17:51:08 +0200

    [Update]: pathfile definition in stats.c

--- sdl/scenes/stats.c
+++ sdl/scenes/stats.c
@@ -5,9 +5,9 @@
 #include "file.h"
 #include "input.h"
 
-/* **************************************************************** */
+#define PATHFILE "stats.conf"
 
-char *pathfile = "../configuration/default.conf";
+/* **************************************************************** */
 
 void stats_init(Conf conf, Scene stats, Assets assets, SDL_Renderer *ren) {
     scene_init(stats, 0, 0, 0, 0, 0, 0, 8);
@@ -16,29 +16,29 @@ void stats_init(Conf conf, Scene stats, Assets assets, SDL_Renderer *ren) {
     (void)ren;
 
     SDL_Rect hitbox = {450, 390, 100, 100};
-    stats->text[0] = text_create(assets, get_setting(pathfile, "4x4"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+    stats->text[0] = text_create(assets, get_setting(PATHFILE, "4x4"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 
     hitbox = (SDL_Rect){450, 530, 100, 100};
-    stats->text[1] = text_create(assets, get_setting(pathfile, "6x6"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+    stats->text[1] = text_create(assets, get_setting(PATHFILE, "6x6"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 
     hitbox = (SDL_Rect){450, 660, 100, 100};
-    stats->text[2] = text_create(assets, get_setting(pathfile, "8x8"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+    stats->text[2] = text_create(assets, get_setting(PATHFILE, "8x8"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 
     hitbox = (SDL_Rect){450, 800, 100, 100};
-    stats->text[3] = text_create(assets, get_setting(pathfile, "10x10"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+    stats->text[3] = text_create(assets, get_setting(PATHFILE, "10x10"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 
     hitbox = (SDL_Rect){450, 930, 100, 100};
-    stats->text[4] = text_create(assets, get_setting(pathfile, "12x12"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+    stats->text[4] = text_create(assets, get_setting(PATHFILE, "12x12"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 
     hitbox = (SDL_Rect){900, 390, 100, 100};
-    stats->text[5] = text_create(assets, get_setting(pathfile, "nb_solved"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+    stats->text[5] = text_create(assets, get_setting(PATHFILE, "nb_solved"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 
     hitbox = (SDL_Rect){900, 530, 100, 100};
-    stats->text[6] = text_create(assets, get_setting(pathfile, "total_time"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+    stats->text[6] = text_create(assets, get_setting(PATHFILE, "total_time"), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 
     hitbox = (SDL_Rect){900, 660, 100, 100};
     stats->text[7] =
-        text_create(assets, (get_setting(pathfile, "overall_accuracy")), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
+        text_create(assets, (get_setting(PATHFILE, "overall_accuracy")), RAJDHANI_MED, LIGHT_BLUE, hitbox, ren);
 }
 
 void stats_process(Conf conf, Scene *scenes, Input input, Assets assets, SDL_Renderer *ren, SDL_Rect win_rect) {
@@ -49,15 +49,14 @@ void stats_process(Conf conf, Scene *scenes, Input input, Assets assets, SDL_Ren
     (void)conf;
     (void)ren;
 
-    scenes[STATS]->text[0] = text_set_text(scenes[STATS]->text[0], get_setting(pathfile, "4x4"), assets, ren);
-    scenes[STATS]->text[1] = text_set_text(scenes[STATS]->text[1], get_setting(pathfile, "6x6"), assets, ren);
-    scenes[STATS]->text[2] = text_set_text(scenes[STATS]->text[2], get_setting(pathfile, "8x8"), assets, ren);
-    scenes[STATS]->text[3] = text_set_text(scenes[STATS]->text[3], get_setting(pathfile, "10x10"), assets, ren);
-    scenes[STATS]->text[4] = text_set_text(scenes[STATS]->text[4], get_setting(pathfile, "12x12"), assets, ren);
-    scenes[STATS]->text[5] = text_set_text(scenes[STATS]->text[5], get_setting(pathfile, "nb_solved"), assets, ren);
-    scenes[STATS]->text[6] = text_set_text(scenes[STATS]->text[6], get_setting(pathfile, "total_time"), assets, ren);
-    scenes[STATS]->text[7] =
-        text_set_text(scenes[STATS]->text[7], get_setting(pathfile, "overall_accuracy"), assets, ren);
+    scenes[STATS]->text[0] = text_set_text(scenes[STATS]->text[0], get_setting(PATHFILE, "4x4"), assets, ren);
+    scenes[STATS]->text[1] = text_set_text(scenes[STATS]->text[1], get_setting(PATHFILE, "6x6"), assets, ren);
+    scenes[STATS]->text[2] = text_set_text(scenes[STATS]->text[2], get_setting(PATHFILE, "8x8"), assets, ren);
+    scenes[STATS]->text[3] = text_set_text(scenes[STATS]->text[3], get_setting(PATHFILE, "10x10"), assets, ren);
+    scenes[STATS]->text[4] = text_set_text(scenes[STATS]->text[4], get_setting(PATHFILE, "12x12"), assets, ren);
+    scenes[STATS]->text[5] = text_set_text(scenes[STATS]->text[5], get_setting(PATHFILE, "nb_solved"), assets, ren);
+    scenes[STATS]->text[6] = text_set_text(scenes[STATS]->text[6], get_setting(PATHFILE, "total_time"), assets, ren);
+    scenes[STATS]->text[7] = text_set_text(scenes[STATS]->text[7], get_setting(PATHFILE, "overall_accuracy"), assets, ren);
 
     SDL_Rect hitbox = {418, 67, 142, 28};
     hitbox = scale_rect(hitbox, win_rect);
